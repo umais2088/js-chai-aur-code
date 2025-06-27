bc82410ed036857e0ec69911b2597667b5298e3f
umais2088 <umaisseox@gmail.com>
2025-06-27 13:59:45 +0000

    datatypes

--- 01_basic/01_datatypes.js
+++ 01_basic/01_datatypes.js
@@ -1,2 +1,6 @@
 "use strict"; // treat the all js code as newer version
 
+//alert is a function that shows an alert box with a message and this used in browser
+//  but we are working using nodejs so we will not use alert.
+// we will make sure code is readable and understandable and we will use proper spacing.
+// console.log("Hello World!") console.log(3+3) // this is a comment and it will not be executed and this is not good way
