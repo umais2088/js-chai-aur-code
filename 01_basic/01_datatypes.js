754f50fd2a5dad62f33c10185e815181c6aacbf3
umais2088 <umaisseox@gmail.com>
2025-06-27 13:27:37 +0000

    container config done

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,2 @@
+"use strict"; // treat the all js code as newer version
+
---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accoundId = 1122;
+let userName = "Umais Riaz";
+var rollNo = 2088;
+degree = "Software Engineering";
+
+// accoundId= 3322;
+userName = "subhan";
+rollNo = 2033;
+degree = "comupter science";
+
+console.table([accoundId,userName,rollNo,degree])
