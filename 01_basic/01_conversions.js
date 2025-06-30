c693aeed1307d3a7cef293ef5bbd1717c68d0e17
Umais Riaz <umaisseox@gmail.com>
2025-06-30 06:10:43 +0000

    conversion

--- 01_basic/01_conversions.js
+++ 01_basic/01_conversions.js
@@ -1,5 +1,5 @@
 // conversions+operations
-let Name= false;
+/* let Name= false;
 let rollNo= 34
 
 let stringInNumber= Number(Name)
@@ -11,3 +11,18 @@ console.log(
         typeof numberString,
     ]
 )
+    */
+let myName= 0
+let stringIsLogedIN = Boolean(myName)
+
+// console.log(stringIsLogedIN)
+//in this i learn how conversion works 
+// if i give 0 to myname => false
+// null=> false
+// undefined=> false
+// string=> true
+let string= false - 8;
+console.log(string);
+console.log(typeof string)
+
+
