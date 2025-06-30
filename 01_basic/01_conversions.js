b5885110f89ad0d2871ac92d0621ab4e65ecb78c
Umais Riaz <umaisseox@gmail.com>
2025-06-30 05:51:57 +0000

    conversion

---
+++ 01_basic/01_conversions.js
@@ -0,0 +1,13 @@
+// conversions+operations
+let Name= false;
+let rollNo= 34
+
+let stringInNumber= Number(Name)
+let numberString= String(rollNo)
+
+console.log(
+    [
+        typeof stringInNumber,
+        typeof numberString,
+    ]
+)
--- 01_basic/01_datatypes.js
+++ 01_basic/01_datatypes.js
@@ -4,3 +4,16 @@
 //  but we are working using nodejs so we will not use alert.
 // we will make sure code is readable and understandable and we will use proper spacing.
 // console.log("Hello World!") console.log(3+3) // this is a comment and it will not be executed and this is not good way
+let name = "Umais";
+let rollNo = 2088;
+let isLogedIn = false;
+let temp = null;
+let Class;
+
+console.log([
+  typeof name,        // "string"
+  typeof rollNo,      // "number"
+  typeof isLogedIn,   // "boolean"
+  typeof temp,        // "object" (known JS quirk: typeof null is "object")
+  typeof Class        // "undefined"
+]);
